b38ccd7213719b1c706d518620db1006238323e8
PANTHER VLOGS <viveksagar7319@gmail.com>
2023-11-08 14:08:42 +0000

    Table align

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -155,6 +155,7 @@ const Sidebar = ({ func }) => {
                 cursor: "pointer",
                 flexWrap: "nowrap",
               }}
+              data-title={item.name}
               onClick={() => navigate(item.route)}
             >
               <item.icon
